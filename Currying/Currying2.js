81ddac50ccdc790a2eddf75e8000dbb705365b8c
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-08-07 01:43:06 +0530

    Code refactor

--- Currying/Currying2.js
+++ Currying/Currying2.js
@@ -9,3 +9,6 @@ const step1 = sendAutoEmail('abhishek.23bcs10065@scaler.com');
 const step2 = step1('Deadline Extension CLI');
 const step3 = step2('Kindly, extend the deadline for the CLI assignment.\nBest regards,\nAbhishek\n');
 console.log(step3);
+
+// Currying heavily relies on closures in js
+// Currying uses the concept of closures to work effectively.
