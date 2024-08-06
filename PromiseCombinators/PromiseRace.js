af5ff33f5cec3b1de2ea725e1328010360dd4c33
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-08-06 15:02:26 +0530

    Promise.race combinator

--- PromiseCombinators/PromiseRace.js
+++ PromiseCombinators/PromiseRace.js
@@ -7,9 +7,20 @@ const promiseThree = new Promise((res, rej) => {
         else res('Promise three is resolved.');
     }, 2000);
 })
-const promiseFour = Promise.reject('Promise four is rejected.');
-const promiseFive = Promise.reject('Promise five is rejected.');
-promiseFour.catch((err) => {
+// const promiseFour = Promise.reject('Promise four is rejected.');
+const promiseFive = Promise.resolve('Promise five is resolved.');
+// promiseFour.catch((err) => {
+//     console.log(err);
+// })
+
+// Promise.race takes an array of promises.
+// Resolves or rejects as soon as the first promise in the array resolves or rejects.
+// Resolves or rejects with the value/reason of the promise 
+// Returns a single promise.
+Promise.race([promiseThree, promiseFive])
+.then((value) => {
+    console.log(value);
+})
+.catch((err) => {
     console.log(err);
 })
-
